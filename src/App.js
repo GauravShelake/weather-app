7aae02be043c1a32202160acd4ae52712638c552
Gaurav_ <gauravshelke2001@gmail.com>
2023-09-22 14:23:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,29 +3,42 @@ import './App.css';
 
 
 function App() {
-  const [data, setData] =useState({})
-  const [location, setLocation] = useState('')
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=96cbb30ceede9333304be1c8e5361059`
-  const searchLocation = (event) => {
-    if(event.key === 'Enter') {
-      useEffect(()=>fetch.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=96cbb30ceede9333304be1c8e5361059`)
-      .then((response) =>{
-        setData(response.data)
-        console.log(response.data)
-      }))
-      setLocation('')
+  const [data, setData] =useState({});
+  const [location, setLocation] = useState('');
+  const fetchData = async () => {
+    try{
+      const res =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=96cbb30ceede9333304be1c8e5361059`);
+      const jsonData = await res.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
     }
-  }
+  };
+  /* eslint-disable */ 
+  useEffect (() =>{
+    if(location) {
+      fetchData();
+    }
+  },[location]);
+  const searchLocation = (event) =>{
+    if(event.key === 'Enter') {
+      fetchData();
+    }
+  };
   return (
     <div className="app">
     <div className="search">
-      <input value={location} onChange={event=> setLocation(event.target.value)} onKeyDown={searchLocation} placeholder='Enter Location' type="text" /></div>      
-    
+      <input value={location} 
+      onChange={(event)=> setLocation(event.target.value)} 
+      onKeyDown={searchLocation} 
+      placeholder='Enter city........' 
+      type="text" />
+      </div>          
     <div className="container">
       <div className="top">
         <div className="location"><p>{data.name}</p></div>
         <div className="temp">
-          {data.main ? <h1>{data.main.temp.toFixed()}</h1> : null}
+          {data.main ? <h1>{data.main.temp}°F</h1> : null}
         </div>
         <div className="description">
           {data.weather ? <p>{data.weather[0].main}</p> :null}
@@ -33,7 +46,7 @@ function App() {
       </div>
       {data.name !== undefined && <div className="bottom">
         <div className="feels">
-          {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}F</p> : null}
+          {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
           <p>Feels Like</p>
         </div>
         <div className="humidity">
